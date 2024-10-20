eee68e419da98ef6a8f14be6b41526dcd7cf3daa
Denis Beresnev <shanthi.bunch@gmail.com>
2024-10-20 23:25:03 +0300

    feat: updated styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Container, Typography, IconButton, Box } from "@mui/material";
+import {
+	Container,
+	Typography,
+	IconButton,
+	Box,
+	useTheme,
+	useMediaQuery,
+	Paper,
+} from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
@@ -45,6 +53,9 @@ const defaultUserVector = [
 ];
 
 const App = () => {
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down("sm")); // Определяем устройство
+
 	const [product, setProduct] = useState(null);
 	const [count, setCount] = useState(0);
 	const userVector =
@@ -105,55 +116,144 @@ const App = () => {
 	}, []);
 
 	console.log(product);
-
 	return (
 		<Container
+			maxWidth="sm"
 			style={{
-				background: "linear-gradient(white, lightgrey)",
-				minHeight: "100vh",
-				padding: "20px",
+				maxHeight: "100vh",
+				height: "100vh",
+				overflow: "hidden",
+				padding: "20px 0",
 			}}
 		>
 			<Box
 				display="flex"
 				justifyContent="space-between"
 				alignItems="center"
+				mb={2}
 			>
-				<IconButton onClick={resetLocalStorage}>
+				<IconButton
+					onClick={resetLocalStorage}
+					style={{ backgroundColor: "#FFFFFF" }}
+				>
 					<RefreshIcon />
 				</IconButton>
-				<IconButton>
+				<Box
+					display="flex"
+					alignItems="center"
+					position="relative"
+					style={{ padding: "20px" }}
+				>
 					<ShoppingCartIcon />
-					<Typography>{count}</Typography>
-				</IconButton>
+					<Box
+						sx={{
+							position: "absolute",
+							top: 5,
+							right: 10,
+							backgroundColor: "#404040",
+							borderRadius: "50%",
+							width: "18px",
+							height: "18px",
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+							fontSize: "12px",
+							color: "white",
+						}}
+					>
+						{count}
+					</Box>
+				</Box>
 			</Box>
+
 			{product ? (
-				<Box textAlign="center">
-					<Typography variant="h5">{product.name}</Typography>
-					<img
-						id="myImage"
-						src={product.image}
-						alt={product.name}
-						style={{ maxWidth: "100%" }}
-					/>
-					<Typography>{product.description}</Typography>
-					<Box display="flex" justifyContent="center" mt={2}>
-						<IconButton onClick={() => updateFeedback(-1)}>
-							<ThumbDownIcon />
-						</IconButton>
-						<IconButton onClick={() => setCount(count + 1)}>
-							<ShoppingCartIcon />
-						</IconButton>
-						<IconButton onClick={() => updateFeedback(1)}>
-							<ThumbUpIcon />
-						</IconButton>
+				<Box textAlign="center" flexGrow={1} overflow="hidden">
+					<Typography variant="h5" gutterBottom>
+						{product.name}
+					</Typography>
+					<Paper
+						elevation={3}
+						style={{
+							padding: "10px",
+							borderRadius: "15px",
+							marginBottom: "20px",
+						}}
+					>
+						<img
+							id="myImage"
+							alt={product.name}
+							style={{
+								maxWidth: "100%",
+								height: "auto",
+								borderRadius: "15px",
+							}}
+						/>
+					</Paper>
+
+					<Box
+						style={{
+							padding: "15px",
+							borderRadius: "15px",
+							boxShadow: "inset 0 0 5px rgba(0, 0, 0, 0.2)",
+							marginBottom: "20px",
+							maxHeight: "100px",
+							overflow: "hidden",
+							textOverflow: "ellipsis",
+						}}
+					>
+						<Typography variant="body2">
+							{product.description}
+						</Typography>
 					</Box>
 				</Box>
 			) : (
 				<Box textAlign="center">
-					<Typography>no products ): </Typography>
+					<Typography>No products ): </Typography>
 				</Box>
 			)}
+
+			<Box
+				display="flex"
+				justifyContent="space-between"
+				alignItems="center"
+				position="absolute"
+				bottom="20px"
+				left="0"
+				right="0"
+				px={5}
+			>
+				<IconButton
+					onClick={() => updateFeedback(-1)}
+					style={{
+						backgroundColor: "#FFCCCC",
+						width: "100px",
+						height: "100px",
+					}}
+				>
+					<ThumbDownIcon />
+				</IconButton>
+				<IconButton
+					onClick={() => setCount(count + 1)}
+					style={{
+						backgroundColor: "#404040",
+						color: "white",
+						width: "60px",
+						height: "60px",
+					}}
+				>
+					<ShoppingCartIcon />
+				</IconButton>
+				<IconButton
+					onClick={() => updateFeedback(1)}
+					style={{
+						backgroundColor: "#CCFFCC",
+						width: "100px",
+						height: "100px",
+					}}
+				>
+					<ThumbUpIcon />
+				</IconButton>
+			</Box>
 		</Container>
 	);
 };
